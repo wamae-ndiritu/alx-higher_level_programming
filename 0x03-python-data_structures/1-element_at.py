64ec577a95bc34eec2daffa461bb8ee28997f86f
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-14 17:56:59 +0000

    Fix trying to access a list out of range

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,7 +4,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     element = my_list[idx]
     return element
