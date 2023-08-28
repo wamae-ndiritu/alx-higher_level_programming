ef09f2dbe1e3a06e75933f58ac4a38807ee637f6
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-28 15:12:25 +0000

    Handle ValueError exception

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value), end="\n")
+    except ValueError:
+        return False
+    else:
+        return True
