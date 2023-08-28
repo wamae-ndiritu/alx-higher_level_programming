a8b561186fa26292c6961db658c39aba78e04c96
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-28 15:15:18 +0000

    Catch TypeError exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value), end="\n")
-    except ValueError:
-        return False
-    else:
         return True
+    except (ValueError, TypeError):
+        return False
