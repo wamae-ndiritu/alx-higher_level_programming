88a1298908eb4314f8261296e597512ea4ffba37
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-29 20:28:26 +0000

    Validate attributes before instantiation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+"""
+Defines a square using class
+"""
+
+
+class Square:
+    """
+    The Square class defines a square based on the
+    provided size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Validates size & Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Attributes:
+            __size (int): The private instance attribute for the size
+        """
+
+        if not isinstance(int, size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
