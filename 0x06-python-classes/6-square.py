e3f5c058e275b90df0c14b64668660962baa63b8
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-29 21:25:42 +0000

    Refactor my_print method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -103,9 +103,9 @@ class Square:
 
         if self.__size == 0:
             print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
+            return
+        for _ in range(self.__position[1]):
+            print()
 
-            for _ in range(self.__size):
-                print(' ' * self.__position[0] + '#' * self.__size)
+        for _ in range(self.__size):
+            print(' ' * self.__position[0] + '#' * self.__size)
