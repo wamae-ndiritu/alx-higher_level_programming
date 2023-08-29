6082814cc0c97b3f2ba1d8fb6a8f032a8fb51da1
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-08-29 20:36:45 +0000

    Create a public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+
+"""
+Defines a square using class
+"""
+
+
+class Square:
+    """
+    The Square class defines a square based on the
+    provided size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Validates size & Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Attributes:
+            __size (int): The private instance attribute for the size
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Gets the area of the square
+        """
+
+        return self.__size ** 2
