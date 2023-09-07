f77a44230b18d024f5a2b8f68f99514c06f51322
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-09-07 08:48:08 +0000

    Update function documentation

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
+"""
+Defines a function to print squares
+"""
 
 
 def print_square(size):
+    """
+    Prints a square of size dimensions
+
+    Args:
+        size (int): The dimensions of the square
+
+    Raises:
+        TypeError: If size is not an int or it is a float and less than 0
+        ValueError: If size is less than 0
+
+    """
+
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if not isinstance(size, int):
