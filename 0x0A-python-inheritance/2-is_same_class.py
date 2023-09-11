aba68bf6e8777887f274fdba4984c1f2ca4e4507
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-09-11 10:42:01 +0000

    Add function to check instances of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines the is_same_class that checks
+whether the object is exactly an instance of
+the specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Args:
+        obj: Represent the object to check
+        a_class: a specified class
+    Return:
+        True: If obj is an instance of class a_class
+        False: If obj is not an instance of a_class
+    """
+
+    return isinstance(obj, a_class)
