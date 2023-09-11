335a3fc0cd83aba054262c7505fdf08cc2da460a
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-09-11 12:18:21 +0000

    Fix checking whether value is an integer

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -29,7 +29,7 @@ class BaseGeometry:
             ValueError: If value is less than or equal to 0
         """
 
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
