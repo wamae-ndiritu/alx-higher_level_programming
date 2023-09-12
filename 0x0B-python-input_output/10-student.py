f02b76bc6b0e4875b352d2bb82e7fceb16a703e1
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-09-12 11:51:38 +0000

    Improve student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Defines class Student
+"""
+
+
+class Student:
+    """
+    class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Instatiates first_name last_name and age
+
+        Args:
+            first_name (str): student's first name
+            last_name (str): student's last name
+            age (int): student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs (list, optional): A list of attribute names
+            to retriev. Defaults to None
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+
+        if attrs is None:
+            return self.__dict__
+
+        data = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                data[attr] = getattr(self, attr)
+        return data
