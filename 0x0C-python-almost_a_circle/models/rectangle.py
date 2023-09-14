64f1046910f4bc75bea070c9cd5efc70a8dfadf2
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-09-14 09:08:37 +0000

    Add area method to Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -163,3 +163,12 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        """
+        Calculates the area of the rectangle
+
+        Returns:
+            int: the area of the rectangle
+        """
+        return self.__width * self.__height
