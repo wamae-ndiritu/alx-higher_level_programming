66a55ed47f32aec06f303f6c716b98efedc6b82e
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-17 20:48:10 +0000

    Insert multiple records in a table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- Create a table and add multiple rows
+CREATE TABLE second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+-- Insert records to second_table
+INSERT INTO second_table(id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
