e5ea21f5a730741dff1a7acc22d066e900acf1fd
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-18 21:38:57 +0000

    Use left join to list genre IDs even for shows with no genre

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,1 +1,5 @@
--- Us
+-- List shows with genre IDs (NULL for shows ithout a genre)
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
