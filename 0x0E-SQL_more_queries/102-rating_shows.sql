eb507c31f0d4810bbb0c52e5ead03fda6a762613
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-18 22:44:21 +0000

    Fix listing all shows by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- List shows by their rating sum in descending order
-SELECT tv_shows.title, SUM(ratings.rating) AS rating_sum
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-LEFT JOIN ratings ON tv_shows.id = ratings.show_id
+INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
