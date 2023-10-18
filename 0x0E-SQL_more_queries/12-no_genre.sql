6599f8721b1514325b96ce88f97c50f44ffa633a
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-18 21:43:36 +0000

    List all shows without a genre linked

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
--- List shows with genre IDs (NULL for shows ithout a genre)
+-- Lists all shows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
