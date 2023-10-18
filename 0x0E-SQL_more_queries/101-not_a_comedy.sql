95fa11f5c50f19c51bb3f331620ccff14898a406
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-18 22:32:56 +0000

    List all shows without the genre Comedy in the database

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,7 @@
+-- List shows without the genre Comedy
+SELECT DISTINCT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name <> 'Comedy' OR tv_genres.name IS NULL
+ORDER BY tv_shows.title ASC;
