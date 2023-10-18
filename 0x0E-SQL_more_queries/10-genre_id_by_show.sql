56349c757bb7caf433be955d633345a1cf4f05b8
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-18 21:33:01 +0000

    Remove whitespaces

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,7 +4,4 @@
 USE hbtn_0d_tvshows;
 
 -- List all shows with linked genres
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
