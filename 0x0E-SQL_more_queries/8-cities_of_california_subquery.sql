e294fc5d0fc9e00a276e67b3e35401d4b9aa28d8
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-18 21:13:32 +0000

    Select from cities and states'

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,2 @@
+-- List all the cities that can be found in the database
+SELECT cities.id, cities.name FROM cities, states WHERE cities.state_id = states.id AND states.name = 'California' ORDER BY cities.id ASC;
