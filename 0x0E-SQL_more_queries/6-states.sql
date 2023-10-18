4b74ca62ea25cbe5fef2316dd304b5c381033b40
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-18 20:54:50 +0000

    Create states table with id as PK

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- Create database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use database hbtn_0d_usa
+USE hbtn_0d_usa;
+
+-- Create table state
+CREATE TABLE IF NOT EXISTS states(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL,
+	UNIQUE KEY (id)
+);
