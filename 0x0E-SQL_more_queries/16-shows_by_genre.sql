92ea7ee6b29d6df9b2f9299fb0921fba1c0193cf
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-18 22:10:55 +0000

    Remove alias

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- List shows and their linked genres
 -- Displays NULL for shows without a genre
-SELECT tv_shows.title, tv_genres.name AS genre
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
