a7a01a0d2bb066904a1234b75473d71388ceeed6
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-18 22:07:17 +0000

    List all shows, and genres linked to that show from a db

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- List shows and their linked genres
+-- Displays NULL for shows without a genre
+SELECT tv_shows.title, tv_genres.name AS genre
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
