b5ce5af0612c62307eb6b51f9f1d9e78b5cd6f8a
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-13 21:29:31 +0000

    Print 0 if number of args < 2

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,11 +8,7 @@ const integers = args.map(Number);
 const sortedInts = integers.sort((a, b) => b - a);
 
 if (sortedInts.length < 2) {
-  if (sortedInts.length === 1) {
-    console.log(1);
-  } else {
-    console.log(0);
-  }
+  console.log(0);
 } else {
   console.log(sortedInts[1]);
 }
