eb2997002c60938bc9c3c46cb491b6b3eaf34faf
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-15 06:47:21 +0000

    Create instance method charPrint for class Square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.width; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += c;
+        }
+        console.log(row);
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = Square;
