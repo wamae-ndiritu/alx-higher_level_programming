f9135e68131d419e85d52509f51219f5f099b8ae
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-19 17:44:40 +0000

    Filtering table records using MySQLdb

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+List all states with name starting with N
+from the database htbn_0e_0_usa
+using MySQLdb
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        exit(1)
+
+    username, password, database = argv[1], argv[2], argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, password=password, db=database)
+
+    cursor = db.cursor()
+
+    # Execute query to receive states from the database
+    cursor.execute("""SELECT * FROM states WHERE name LIKE
+                   'N%' ORDER BY states.id ASC""")
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # close the cursor and db connection
+    cursor.close()
+    db.close()
