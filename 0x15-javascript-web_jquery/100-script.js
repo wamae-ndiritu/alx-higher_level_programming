66a9cb0c7368003c730d13f7900b97517cfd52af
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-01-11 05:50:10 +0000

    Change the text color before the content is loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+	// Select the <header> element and update its text color
+	var headerElement = document.querySelector('header');
+	if (headerElement) {
+		headerElement.style.color = '#FF0000';
+	}
+});
